2ea720538329cfc6fbe32b77f818c744f711bedc
Avshalom Amossi <amossi.av@gmail.com>
2021-09-03 17:50:59 +0300

    Changed the name to saleet #2

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 This python project is designed to test all Git aspects
 Added another line to the comments
 """
-from functions import printName
+from functions import *
 
 if __name__ == '__main__':
     printName('Saleet')
+

b47b44a84f3ab7dd0ce21f6b82efdaee960d1389
Avshalom Amossi <amossi.av@gmail.com>
2021-09-03 12:45:53 +0300

    Added the first function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
 This python project is designed to test all Git aspects
 """
-from functions import printName
+from functions import *
 
 if __name__ == '__main__':
     printName()
+    print(calcValue(4,7))
